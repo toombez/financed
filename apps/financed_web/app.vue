dd79d24d03d4e6b311f7a8d7f21b05baf3af1b98
Timur <toombez.timurtokaev@gmail.com>
2025-04-04 10:03:15 +0300

    Init core crate

--- apps/financed_web/app.vue
+++ apps/financed_web/app.vue
@@ -1,3 +1,12 @@
+<script setup lang="ts">
+import init, { a } from 'financed_core'
+onBeforeMount(async () => {
+    await init()
+
+    console.log(a())
+})
+</script>
+
 <template>
   <div>
     <NuxtRouteAnnouncer />
---
+++ crates/financed_core/src/lib.rs
@@ -0,0 +1,6 @@
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+pub fn a() -> String {
+    return "b".to_string()
+}
